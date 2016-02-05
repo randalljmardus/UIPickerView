c4d30dfaa003fc82e3d700d8c0495091c379983c
Randall Mardus <randalljmardus@gmail.com>
2016-02-05 11:04:00 -0500

    first commit

--- UIPickerView/ViewController.swift
+++ UIPickerView/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var selectionLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let pickerData = ["iPad", "iPhone", "Gameboy", "PS3", "Sega Genesis", "Dreamcast"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    pickerView.dataSource = self
+    pickerView.delegate = self
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectionLabel.text = "Your favorite console is \(pickerData[row])"
+    }
+    
+    
+    
+    
+    
 
 }
 
